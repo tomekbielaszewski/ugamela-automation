ec47195c9bd42efbd186bf9f329a4608287db9f0
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2022-08-10 22:25:54 +0200

    choosing planets by names from dropdowns

--- src/main/java/pl/grizwold/ugamela/page/Fleet2.java
+++ src/main/java/pl/grizwold/ugamela/page/Fleet2.java
@@ -2,15 +2,17 @@ package pl.grizwold.ugamela.page;
 
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import pl.grizwold.ugamela.page.model.Address;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Fleet2 extends Page {
     private static final String FLEET_2_PAGE = "fleet1.php";
     private static final String OWN_DESTINATION_DROP_DOWN_SELECTOR = "#fl_sel1";
-    private static final String OWN_DESTINATION_OPTION_SELECTOR = "#fl_sel1 > option:nth-child(%d)";
+    private static final String OWN_DESTINATION_OPTION_SELECTOR = "//*[@id=\"fl_sel1\"]/option[contains(text(), \"%s\")]";
     private static final String ADDRESS_GALAXY_SELECTOR = "#galaxy_selector";
     private static final String ADDRESS_SYSTEM_SELECTOR = "#system_selector";
     private static final String ADDRESS_PLANET_SELECTOR = "#select_planet";
@@ -32,10 +34,15 @@ public class Fleet2 extends Page {
         super(parent.session);
     }
 
-    public Fleet2 selectDestinationColony(int index) {
+    public Fleet2 selectDestinationColony(String planetNameSubstring) {
         validateState();
         $().findElement(By.cssSelector(OWN_DESTINATION_DROP_DOWN_SELECTOR)).click();
-        $().findElement(By.cssSelector(String.format(OWN_DESTINATION_OPTION_SELECTOR, index))).click();
+        List<WebElement> options = $().findElements(By.xpath(String.format(OWN_DESTINATION_OPTION_SELECTOR, planetNameSubstring)));
+        if(options.isEmpty())
+            throw new IllegalStateException("No matching planets found for: " + planetNameSubstring);
+        if(options.size() > 1)
+            throw new IllegalStateException("Too many planets found for: " + planetNameSubstring);
+        options.get(0).click();
         return this;
     }
 
--- src/main/java/pl/grizwold/ugamela/page/PlanetChooser.java
+++ src/main/java/pl/grizwold/ugamela/page/PlanetChooser.java
@@ -1,8 +1,10 @@
 package pl.grizwold.ugamela.page;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import pl.grizwold.ugamela.UgamelaSession;
+
+import java.util.List;
 
 public class PlanetChooser extends Page {
 
@@ -13,5 +15,15 @@ public class PlanetChooser extends Page {
     public void openPlanet(int planetNumber) {
         $().findElement(By.cssSelector("#planet")).click();
         $().findElement(By.cssSelector("#planet > option:nth-child(" + planetNumber + ")")).click();
+    }
+
+    public void openPlanet(String nameSubstring) {
+        $().findElement(By.cssSelector("#planet")).click();
+        List<WebElement> options = $().findElements(By.xpath("//*[@id=\"planet\"]/option[contains(text(), \"" + nameSubstring + "\")]"));
+        if(options.isEmpty())
+            throw new IllegalStateException("No matching planets found for: " + nameSubstring);
+        if(options.size() > 1)
+            throw new IllegalStateException("Too many planets found for: " + nameSubstring);
+        options.get(0).click();
     }
 }
